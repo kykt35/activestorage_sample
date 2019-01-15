a11ebc336f64797c039fc77e890ae495f0403814
kykt35 <kykt35@gmail.com>
2019-01-15 15:20:35 +0900

    Edit to add image_box etc

--- app/assets/javascripts/item_images.js
+++ app/assets/javascripts/item_images.js
@@ -1,11 +1,12 @@
 $(document).on('turbolinks:load', function() {
 
-  $('.file_input').on('change',function(e){
+  $('#item_images').on('change',function(e){
     var files = e.target.files;
     var d = (new $.Deferred()).resolve();
     $.each(files,function(i,file){
       d = d.then(function(){return previewImage(file)});
     });
+    $('#item_images').val('');
   })
 
   function previewImage(imageFile){
@@ -14,9 +15,13 @@ $(document).on('turbolinks:load', function() {
     var def =$.Deferred();
     reader.onload = function(e){
       img.onload = function(){
+        var image_box = $('<div>',{class: 'image-box'}).css("width", "150px");
         $(img).css('width','150px');
-        $('.images_field').append(img);
-        def.resolve(img);
+        image_box.append(img);
+        image_box.append($('<p>').html(imageFile.name));
+        image_box.append('<a href="" class= "btn-edit">Edit</a>','<a href="" class="btn-delete">Delete</a>');
+        $('.images_field').append(image_box);
+        def.resolve();
       };
       img.src = e.target.result;
     };
