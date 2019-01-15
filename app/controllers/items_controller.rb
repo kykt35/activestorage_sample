9d6f3f51745def71194403fd846273c59bbcc2bc
kykt35 <kykt35@gmail.com>
2019-01-15 21:04:04 +0900

    Edit items_controller add update image etc

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all
+    @items = Item.all.with_attached_images
   end
 
   def show
@@ -17,6 +17,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    uploaded_images.each {|image| @item.images.attach(image)} if uploaded_images.present?
     if @item.save
       redirect_to @item, notice: 'Item was successfully created.'
     else
@@ -25,6 +26,8 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item.images.detach
+    uploaded_images.each {|image| @item.images.attach(image)} if uploaded_images.present?
     if @item.update(item_params)
       redirect_to @item, notice: 'Item was successfully updated.'
     else
@@ -37,13 +40,30 @@ class ItemsController < ApplicationController
       redirect_to items_url, notice: 'Item was successfully destroyed.'
   end
 
+  def upload_image
+      @image_blob = create_blob(params[:image])
+      respond_to do |format|
+        format.json { @image_blob.id }
+      end
+  end
+
+
   private
 
-    def set_item
-      @item = Item.find(params[:id])
-    end
+  def set_item
+    @item = Item.with_attached_images.find(params[:id])
+  end
 
-    def item_params
-      params.require(:item).permit(:name, :price, :description, images: [])
-    end
+  def item_params
+    params.require(:item).permit(:name, :price, :description)
+  end
+  def uploaded_images
+    params[:item][:images].map{|id| ActiveStorage::Blob.find(id)} if params[:item][:images]
+  end
+  def create_blob(uploading_file)
+    ActiveStorage::Blob.create_after_upload! \
+      io: uploading_file.open,
+      filename: uploading_file.original_filename,
+      content_type: uploading_file.content_type
+  end
 end
