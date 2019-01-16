d6da9914c94845edad828218966bd37792a147e6
kykt35 <kykt35@gmail.com>
2019-01-16 18:58:59 +0900

    Edit item_image.js

--- app/assets/javascripts/item_images.js
+++ app/assets/javascripts/item_images.js
@@ -1,34 +1,37 @@
 $(document).on('turbolinks:load', function() {
 
   $('#item_images').on('change',function(e){
-    var file = e.target.files[0];
-    Uploader.upload(file).done(function(data){
-      console.log("done");
-      previewImage(file, data.image_id);
+    var files = e.target.files;
+    var d = (new $.Deferred()).resolve();
+    $.each(files,function(i,file){
+      d = d.then(function() {
+          return Uploader.upload(file)})
+        .then(function(data){
+          return previewImage(file, data.image_id);
+      });
     });
     $('#item_images').val('');
   });
 
-  $('.images_field').on('click','.btn-edit', function(e){
+  $('.images-field').on('click','.btn-edit', function(e){
     e.preventDefault();
     $(this).parent().find('.edit-image-file-input').trigger("click");
   });
 
-  $('.images_field').on('change','.edit-image-file-input', function(e){
+  $('.images-field').on('change','.edit-image-file-input', function(e){
     var file = e.target.files[0];
     var image_box = $(this).parent();
     Uploader.upload(file).done(function(data){
-      console.log("edit done");
       replaceImage(file, data.image_id, image_box);
     });
   });
 
-  $('.images_field').on('click','.btn-delete', function(e){
+  $('.images-field').on('click','.btn-delete', function(e){
     e.preventDefault();
     $(this).parent().remove();
   });
 
-  function replaceImage(imageFile, image_id, element){
+  var replaceImage = function(imageFile, image_id, element){
     var reader = new FileReader();
     var img = element.find('img');
     var input = element.find('.item-images-input');
@@ -37,37 +40,37 @@ $(document).on('turbolinks:load', function() {
       input.attr({value: image_id});
       filename.html(imageFile.name);
       img.attr("src", e.target.result);
-    }
+    };
     reader.readAsDataURL(imageFile);
   }
 
-  function previewImage(imageFile, image_id){
+  var previewImage = function(imageFile, image_id){
     var reader = new FileReader();
     var img = new Image();
+    var def =$.Deferred();
     reader.onload = function(e){
-      img.onload = function(){
-        var image_box = $('<div>',{class: 'image-box'}).css("width", "150px");
-        $(img).css('width','150px');
-        image_box.append(img);
-        image_box.append($('<p>').html(imageFile.name));
-        image_box.append($('<input>').attr({
-              name: "item[images][]",
-              value: image_id,
-              style: "display: none;",
-              type: "hidden",
-              class: "item-images-input"}));
-        image_box.append('<a href="" class= "btn-edit">Edit</a>');
-        image_box.append($('<input>').attr({
-              name: "edit-image[]",
-              style: "display: none;",
-              type: "file",
-              class: "edit-image-file-input file-input"}));
-        image_box.append('<a href="" class="btn-delete">Delete</a>');
-        $('.images_field').append(image_box);
-      };
+      var image_box = $('<div>',{class: 'image-box'});
+      image_box.append(img);
+      image_box.append($('<p>').html(imageFile.name));
+      image_box.append($('<input>').attr({
+            name: "item[images][]",
+            value: image_id,
+            style: "display: none;",
+            type: "hidden",
+            class: "item-images-input"}));
+      image_box.append('<a href="" class= "btn-edit">Edit</a>');
+      image_box.append($('<input>').attr({
+            name: "edit-image[]",
+            style: "display: none;",
+            type: "file",
+            class: "edit-image-file-input file-input"}));
+      image_box.append('<a href="" class="btn-delete">Delete</a>');
+      $('.images-field').append(image_box);
       img.src = e.target.result;
+      def.resolve();
     };
     reader.readAsDataURL(imageFile);
+    return def.promise();
   }
 
   var Uploader = {
